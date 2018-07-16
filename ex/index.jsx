59baf20807c916ae5c4a000538bc251bb4134d3a
natansevero <natanseverolp@gmail.com>
2018-07-15 22:36:54 -0300

    Importando combineReducers e createStore para combinar e criar o store. Provider para passar o store para os componentes

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,8 +1,26 @@
+/*
+   createStore: Serve para criar o store
+   combineReducers: Combina todos os reducers. Cada reducer apontando para uma propriedade
+   do objeto de Store. No fim, ele gera um objeto que será o Store. (Reducers combinados geram o store)   
+   
+   Provider: Consegue pegar o estado e passar para o componentes
+*/
+
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import Field from './field'
 
+const reducers = combineReducers({
+    field: () => ({ value: 'Opa' })
+})
+
 ReactDOM.render(
-    <Field initialValue="teste" />,
+    <Provider store={createStore(reducers)}>
+        <Field initialValue="teste" />
+    </Provider>,
     app
 )
